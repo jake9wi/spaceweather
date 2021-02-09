1df94b2174a344e066e390cfa0ead566fec24c10
Jacob Edwards Wiese <jake9wi@outlook.com>
2021-02-08 18:33:16 -0600

    add predictions

--- src/flux107.py
+++ src/flux107.py
@@ -6,20 +6,49 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import requests
 import pandas as pd
+import datetime as dt
 
-r = requests.get('https://services.swpc.noaa.gov/json/f107_cm_flux.json')
+robsv = requests.get(
+    'https://services.swpc.noaa.gov/json/f107_cm_flux.json'
+)
 
-r.raise_for_status()
+rpred = requests.get(
+    'https://services.swpc.noaa.gov/json/predicted_f107cm_flux.json'
+)
+
+robsv.raise_for_status()
+rpred.raise_for_status()
 
 datecols = [
     'time_tag',
     'avg_begin_date',
 ]
 
-data = pd.read_json(
-    r.text,
+obsv = pd.read_json(
+    robsv.text,
     convert_dates=datecols,
 )
+
+
+PredRaw = pd.read_json(
+    rpred.text,
+    convert_dates=datecols,
+)
+
+PredDict = {
+    'dtg': [
+        PredRaw.iloc[0,0] + dt.timedelta(days=1),
+        PredRaw.iloc[0,0] + dt.timedelta(days=2),
+        PredRaw.iloc[0,0] + dt.timedelta(days=3),
+    ],
+    'value':[
+        PredRaw.iloc[0,1],
+        PredRaw.iloc[0,2],
+        PredRaw.iloc[0,3],
+    ],
+}
+
+pred = pd.DataFrame(PredDict)
 
 ###
 
@@ -30,27 +59,37 @@ fig = plt.figure(
     figsize=(10, 10),
 )
 
-fig.suptitle("10.7 centimetre Flux")
+fig.suptitle("10.7 centimetre Flux\nSamp Rate: 3x (midday)")
 
 ax = plt.gca()
 
 ax.bar(
-    data['time_tag'],
-    data['flux'],
+    obsv['time_tag'],
+    obsv['flux'],
+    label='Observed',
 )
 
-ax.set_xlabel("Time")
+ax.bar(
+    pred['dtg'],
+    pred['value'],
+    color='red',
+    label='Predicted',
+)
+
+ax.set_xlabel("Time (UT?)")
 ax.set_ylabel("Solar Flux Units (sfu)")
 
 ax.set_ylim(
     [
-        data['flux'].min(),
-        data['flux'].max() + 1,
+        obsv['flux'].min(),
+        obsv['flux'].max() + 1,
     ],
 )
 
 ax.xaxis.set_major_formatter(mdates.DateFormatter("%b-%d"))
 ax.xaxis.set_minor_formatter(mdates.DateFormatter("%b-%d"))
+
+ax.legend()
 
 fig.savefig('../web/img/flux107.svg')
 
