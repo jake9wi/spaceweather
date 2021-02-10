9a71f07290b89401892ca22ad0efef731333fb6b
Jacob Edwards Wiese <jake9wi@outlook.com>
2021-02-09 20:03:57 -0600

    only plot noon val. calc min max for all datasets.

--- src/flux107.py
+++ src/flux107.py
@@ -8,47 +8,56 @@ import requests
 import pandas as pd
 import datetime as dt
 
-robsv = requests.get(
-    'https://services.swpc.noaa.gov/json/f107_cm_flux.json'
-)
 
-rpred = requests.get(
-    'https://services.swpc.noaa.gov/json/predicted_f107cm_flux.json'
-)
+def obsv():
+    robsv = requests.get(
+        'https://services.swpc.noaa.gov/json/f107_cm_flux.json'
+    )
 
-robsv.raise_for_status()
-rpred.raise_for_status()
+    robsv.raise_for_status()
 
-datecols = [
-    'time_tag',
-    'avg_begin_date',
-]
+    datecols = [
+        'time_tag',
+    ]
 
-obsv = pd.read_json(
-    robsv.text,
-    convert_dates=datecols,
-)
+    obsv = pd.read_json(
+        robsv.text,
+        convert_dates=datecols,
+    )
+
+    return obsv.loc[obsv['reporting_schedule'] == 'Noon']
 
 
-PredRaw = pd.read_json(
-    rpred.text,
-    convert_dates=datecols,
-)
+def pred():
+    rpred = requests.get(
+        'https://services.swpc.noaa.gov/json/predicted_f107cm_flux.json'
+    )
 
-PredDict = {
-    'dtg': [
-        PredRaw.iloc[0,0] + dt.timedelta(days=1),
-        PredRaw.iloc[0,0] + dt.timedelta(days=2),
-        PredRaw.iloc[0,0] + dt.timedelta(days=3),
-    ],
-    'value':[
-        PredRaw.iloc[0,1],
-        PredRaw.iloc[0,2],
-        PredRaw.iloc[0,3],
-    ],
-}
+    rpred.raise_for_status()
 
-pred = pd.DataFrame(PredDict)
+    PredRaw = pd.read_json(
+        rpred.text,
+    )
+
+    PredDict = {
+        'dtg': [
+            PredRaw.iloc[0,0] + dt.timedelta(days=1),
+            PredRaw.iloc[0,0] + dt.timedelta(days=2),
+            PredRaw.iloc[0,0] + dt.timedelta(days=3),
+        ],
+        'value':[
+            PredRaw.iloc[0,1],
+            PredRaw.iloc[0,2],
+            PredRaw.iloc[0,3],
+        ],
+    }
+
+    pred = pd.DataFrame(PredDict)
+
+    return pred
+
+obsv = obsv()
+pred = pred()
 
 ###
 
@@ -63,10 +72,21 @@ fig.suptitle("10.7 centimetre Flux\nSamp Rate: 3x (midday)")
 
 ax = plt.gca()
 
+ax.step(
+    obsv['time_tag'],
+    obsv['ninety_day_mean'],
+    zorder=3,
+    label='ninety_day_mean',
+    color='green',
+)
+
 ax.bar(
     obsv['time_tag'],
     obsv['flux'],
     label='Observed',
+    zorder=2,
+    lw=0,
+    width=0.5,
 )
 
 ax.bar(
@@ -74,6 +94,9 @@ ax.bar(
     pred['value'],
     color='red',
     label='Predicted',
+    zorder=1,
+    linewidth=0,
+    width=0.5,
 )
 
 ax.set_xlabel("Time (UT?)")
@@ -81,8 +104,17 @@ ax.set_ylabel("Solar Flux Units (sfu)")
 
 ax.set_ylim(
     [
-        obsv['flux'].min(),
-        obsv['flux'].max() + 1,
+        min([
+            obsv['flux'].min(),
+            obsv['ninety_day_mean'].min(),
+            pred['value'].min(),
+        ]) - 1,
+
+        max([
+            obsv['flux'].max(),
+            obsv['ninety_day_mean'].max(),
+            pred['value'].max(),
+        ]) + 1,
     ],
 )
 
